59ab76ba83bd99ae4866ea4d024e8f4e3a08351d
rafaelpinheiro84 <rafaelsp0584@gmail.com>
2022-06-21 15:03:14 -0300

    UC13 Banco De dados

---
+++ AtividadeOnline3/AtividadeOnline3.sql
@@ -0,0 +1,81 @@
+--DDL
+CREATE DATABASE RpgOnlineFst1
+GO
+
+USE RpgOnlineFst1
+GO
+
+CREATE TABLE Usuarios(
+    UsuarioId INT PRIMARY KEY IDENTITY,
+	Email VARCHAR (100) UNIQUE NOT NULL,
+	Senha VARCHAR(50) NOT NULL
+)
+GO
+
+CREATE TABLE Classes(
+   ClasseId INT PRIMARY KEY IDENTITY,
+   Nome VARCHAR(50) UNIQUE NOT NULL,
+   Descricao VARCHAR(255)
+)
+GO
+
+CREATE TABLE Personagens(
+   PersongemId INT PRIMARY KEY IDENTITY,
+   NomePersonagem VARCHAR(30) UNIQUE NOT NULL,
+   UsuarioId INT UNIQUE FOREIGN KEY REFERENCES Usuarios(UsuarioId),
+   ClasseId INT FOREIGN KEY REFERENCES Classes(ClasseId)
+)
+GO
+
+CREATE TABLE Habilidades(
+   HabilidadeId INT PRIMARY KEY IDENTITY,
+   Nome VARCHAR(50) UNIQUE NOT NULL
+)
+GO
+
+CREATE TABLE ClasseHabilidade(
+    ClasseId INT FOREIGN KEY REFERENCES Classes(ClasseId),
+	HabilidadeId INT FOREIGN KEY REFERENCES Habilidades(HabilidadeId)
+)
+
+--DML
+INSERT INTO Usuarios VALUES ('email@email.com', '123456')
+INSERT INTO Classes VALUES ('Bárbaro', 'Descrição do Bárbaro')
+INSERT INTO Habilidades VALUES ('Lança Mortal'), ('Escudo Supremo')
+INSERT INTO Personagens (NomePersonagem, UsuarioId, ClasseId) VALUES ('DeuBug', 1, 1)
+INSERT INTO ClasseHabilidade (ClasseId, HabilidadeId) VALUES (1,1), (1,2)
+
+INSERT INTO Usuarios VALUES ('email2@email.com', '123456')
+INSERT INTO Classes VALUES ('Monge', 'Descrição do Monge')
+INSERT INTO Habilidades VALUES ('Recuperar Vida')
+INSERT INTO Personagens (NomePersonagem, UsuarioId, ClasseId) VALUES ('BitBug', 2, 2)
+INSERT INTO ClasseHabilidade (ClasseId, HabilidadeId) VALUES (2,2), (2,3)
+
+UPDATE Usuarios
+SET Senha = '654321'
+WHERE UsuarioId = 1
+
+
+--DQL
+SELECT * FROM Usuarios
+SELECT * FROM Personagens
+SELECT * FROM Classes
+SELECT * FROM Habilidades
+
+SELECT P.NomePersonagem, C.Nome 'Nome da Classe', C.Descricao, U.Email
+FROM Personagens AS P
+
+JOIN Classes AS C ON P.ClasseId = C.ClasseId
+JOIN Usuarios as U on P.UsuarioId = U.UsuarioId
+
+SELECT u.Email 'Email do usuário', p.NomePersonagem 'Nome personagem', c.Nome 'Classe', c.Descricao 'Descrição' 
+FROM Usuarios u 
+INNER JOIN Personagens p
+ON u.UsuarioId = p.PersonagemId
+INNER JOIN Classes c
+ON p.PersonagemId = c.ClasseId
+
+SELECT u.Email 'Email do usuário', p.NomePersonagem 'Nome personagem', c.Nome 'Classe', c.Descricao 'Descrição' 
+FROM Usuarios u, Personagens p, Classes c
+WHERE u.UsuarioId = p.PersonagemId AND
+      p.PersonagemId = c.ClasseId
